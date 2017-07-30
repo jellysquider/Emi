e0cd25e8cb7b27de6e3cfce61953934394f52541
Martin Stava <martin@strv.com>
2017-07-29 20:12:20 -0700

    Delete demo

--- demo-javascript-todo-master/lib/fetch-todo.js
+++
@@ -1,5 +0,0 @@
-import { todos } from './client'
-
-export default async id => {
-  return await todos.document(id).fetch()
-}
--- demo-javascript-todo-master/lib/update-todo.js
+++
@@ -1,5 +0,0 @@
-import { todos } from './client'
-
-export default async (id, update) => {
-  await todos.document(id).merge(update)
-}
