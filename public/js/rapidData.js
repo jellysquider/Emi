4a9d00f17e721b83be7d69cc72a283caf096c37a
Gabriela <Gabriela>
2017-07-30 07:11:38 -0700

    Added merging, transition away from rapid io being on the backend is almost complete

--- public/js/rapidData.js
+++ public/js/rapidData.js
@@ -1,6 +1,8 @@
+const RAPID_TODO_COLLECTION_NAME = 'MediEmi';
+
 //module.exports {
 
-function stuff() {
+function profileData() {
   console.log("hello!")
   var socket = io.connect('http://localhost:3000');
   var uuid = uuidv4(); // -> v4 UUID
@@ -21,24 +23,116 @@ function stuff() {
   console.log("uuid:" + uuid + " :" + userID);
   //myStorage = window.localStorage;
   console.log("Hi!");
-  $('#contactForm').submit(function(e){
-    e.preventDefault();
-    console.log("yaay!");
+  var totalData = {};
+  totalData.uuid = userID;
+  $('#curStatus').click(function(e){
+    //e.preventDefault();
     console.log("button clicked!");
-    var data = $("#contactForm :input");//.serializeArray();
-    //alert(data);
-    var values = {
-                name: data[0].value,
-                email: data[1].value,
-                phone: data[2].value,
-                msg: data[3].value
-    }
-    socket.emit('contactUs', values);
-    console.log("sent! and button data:" + data[0].value);
-    //$('#success').val('');
-    return false;
+    var idClicked = e.target.id;
+    totalData.status = idClicked;
+    console.log(totalData.status);
+  });
+  $('#originCountry').change(function(e){
+    //e.preventDefault();
+    console.log("button clicked!");
+    totalData.originCountry = $("#originCountry option:selected").text();
+    console.log(totalData.originCountry);
+  });
+  $('#occupation').change(function(e){
+    //e.preventDefault();
+    console.log("button clicked!");
+    totalData.occupation = $("#occupation option:selected").text();
+    console.log(totalData.occupation);
+  });
+  $('#DOB').change(function(e){
+    //e.preventDefault();
+    console.log("DOB entered!");
+    totalData.DOB = $("#DOB").val();
+    console.log(totalData.DOB);
+  });
+  $('#gender').change(function(e){
+    //e.preventDefault();
+    console.log("button clicked!");
+    totalData.gender = $("#gender option:selected").text();
+    console.log(totalData.gender);
+  });
+  $('#famSize').click(function(e){
+    //e.preventDefault();
+    console.log("button clicked!");
+    var idClicked = e.target.id;
+    totalData.famSize = idClicked;
+    console.log(totalData.famSize);
+  });
+  $('#submitBtn').click(function(e){
+    //e.preventDefault();
+    console.log("button clicked!");
+    saveProfileData(totalData);
   });
 
+
+}
+
+function saveProfileData(msg) {
+  console.log("saveProfileData:" + msg);
+
+  client
+    .collection(RAPID_TODO_COLLECTION_NAME)
+    .filter({ and: [
+            { $id: msg.uuid }
+            ]})
+    .fetch(userData =>  {
+      //socket.emit("userData", userData[0].body);
+      try {
+        console.log(JSON.stringify(userData[0].body));
+        console.log(JSON.stringify(msg));
+        var tmp = deepmerge(msg, userData[0].body);
+        msg = tmp;
+        console.log(JSON.stringify(msg));
+      }
+      catch(e) {
+        console.log(e.name + ': ' + e.message);
+      }
+
+      client
+        .collection(RAPID_TODO_COLLECTION_NAME)
+        .document(msg.uuid)
+        .mutate({ // create a new to-do and assign it to John
+          name: msg.name,
+          email: msg.email,
+          phone: msg.phone,
+          gender: msg.gender,
+          canDrive: msg.canDrive,
+          isDisabled: msg.isDisabled,
+          isMinority: msg.isMinority,
+          isLiterate: msg.isLiterate,
+          status: msg.status,
+          originCountry: msg.originCountry,
+          isEducated: msg.isEducated,
+          priority: msg.priority,
+          famSize: msg.famSize,
+          DOB: msg.DOB,
+          occupation: msg.occupation
+      })
+      .then(
+        () => console.log('success'),
+        err => {
+          if (err) {
+            console.log(err.message);
+            switch (err.type) {
+              case 'timeout': break // mutation timed out
+              case 'permission-denied': break // access control related error
+            }
+          }
+        })
+
+    }, error => {
+      // once the error block is called the subscription is automatically canceled
+      // and will no longer receive and updates
+      console.log(err.message)
+      if (err.type === 'permission-denied') {
+        console.log(err.message) // you are not allowed to access data
+      }
+    })
 }
 
 function contactUs() {
